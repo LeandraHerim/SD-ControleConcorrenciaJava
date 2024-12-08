c423b9c1c3098b7be20e458cecd42c235e21de70
Herim Leandra dos Santos <60557693+LeandraHerim@users.noreply.github.com>
2024-12-08 22:16:51 +0000

    versão7 - correções de codigo

---
+++ Atividade/CarroLock.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class CarroLock extends Thread {
+  private Lock estacionamento = new ReentrantLock();
+  
+  public CarroLock(String nome) { 
+    super(nome); 
+  }
+  
+  public void run() {
+    try {
+      estacionamento.lock();
+      System.out.println(getName() + " ocupou vaga.");
+      sleep((long)(Math.random() * 10000));
+      System.out.println(getName() + " liberou vaga.");
+      estacionamento.unlock();
+    } catch(InterruptedException ie){ 
+      ie.printStackTrace(); 
+    }
+  }
+
+  public static void main(String args[]) {
+    for (int i = 0; i < 20; i++)
+      new CarroLock("Carro #" + i).start();
+  }
+}
